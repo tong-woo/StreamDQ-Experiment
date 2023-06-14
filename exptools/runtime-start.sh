d2fb38b58813d223e9daf960e9a155096ae07abd
Tong <tong.wu4@student.uva.nl>
2023-06-14 19:19:47 +0200

    fix: complete runtime-start.sh

--- exptools/runtime-start.sh
+++ exptools/runtime-start.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 cd ../target
 #for reddit
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/20M_reddit_posts.csv -s 1000 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/40M_reddit_posts.csv -s 1000 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/60M_reddit_posts.csv -s 1000 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/80M_reddit_posts.csv -s 1000 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/100M_reddit_posts.csv -s 1000 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/20M_reddit_posts.csv -s 1000 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/40M_reddit_posts.csv -s 1000 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/60M_reddit_posts.csv -s 1000 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/80M_reddit_posts.csv -s 1000 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/100M_reddit_posts.csv -s 1000 -r 7
 
 #for wiki click stream
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/2M_clickstream_enwiki-2023-04.csv -s 100 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/4M_clickstream_enwiki-2023-04.csv -s 100 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/6M_clickstream_enwiki-2023-04.csv -s 100 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/8M_clickstream_enwiki-2023-04.csv -s 100 -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/10M_clickstream_enwiki-2023-04.csv -s 100 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/2M_clickstream_enwiki-2023-04.csv -s 100 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/4M_clickstream_enwiki-2023-04.csv -s 100 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/6M_clickstream_enwiki-2023-04.csv -s 100 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/8M_clickstream_enwiki-2023-04.csv -s 100 -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.RunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/ClickStream/10M_clickstream_enwiki-2023-04.csv -s 100 -r 7
 
 #for baseline experiment
 
 #reddit
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/20M_reddit_posts.csv -c score -r 7
-#kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/40M_reddit_posts.csv -c score -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/20M_reddit_posts.csv -c score -r 7
+kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/40M_reddit_posts.csv -c score -r 7
 kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/60M_reddit_posts.csv -c score -r 7
 kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/80M_reddit_posts.csv -c score -r 7
 kotlin -cp streamdqexp-0.0.1-SNAPSHOT.jar com.tong.streamdqexp.BaselineRunTimeExperimentApp -p /Users/wutong/Desktop/experiment/dataset/reddit_posts/100M_reddit_posts.csv -c score -r 7
